d7a2b875d7b7f112dbceee839b46b033a2f0625c
Rongxuan <rlii0065@student.monash.edu>
2022-07-27 12:22:17 +1000

    First commit

---
+++ src/main/java/com/jrp/pma/controller/ProjectController.java
@@ -0,0 +1,42 @@
+package com.jrp.pma.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jrp.pma.dao.ProjectRepository;
+import com.jrp.pma.entities.Project;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectController {
+
+	@Autowired
+	ProjectRepository proRepo;
+
+	@GetMapping("")
+	public String displayProject(Model model) {
+		List<Project> projects = proRepo.findAll();
+		model.addAttribute("projectsList", projects);
+		return "projects/list-project";
+	}
+
+	@GetMapping("/new")
+	public String displayProjectForm(Model model) {
+		Project aProject = new Project();
+		model.addAttribute("project", aProject);
+		return "projects/new-project";
+	}
+
+	@PostMapping("/save")
+	public String createProject(Project project, Model model) {
+		proRepo.save(project);
+
+		return "redirect:/projects/new";
+	}
+}
